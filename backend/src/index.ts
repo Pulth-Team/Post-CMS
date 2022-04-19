3bab67ec30c9da6f221a7dfde30b5b2a0852aed8
Bekir Gulestan <gulestanbekir@gmail.com>
2022-04-20 00:45:13 +0300

    Add Errors and Error Handling Mechanism

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,16 @@
 import express from "express";
+import "express-async-errors";
+
+import { NotFoundError } from "./errors/not-found-error";
+import { errorHandler } from "./middlewares/error-handler";
 
 const app = express();
 
-app.all("*", (req, res) => {
-  res.send({});
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
 });
+
+app.use(errorHandler);
 
 app.listen(4000, () => {
   console.log("API listening on port 4000");
