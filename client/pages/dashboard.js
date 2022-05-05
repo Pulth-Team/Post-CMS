9dc516d942e1edde67a1e66559da5dbc2455e9b0
Deniz <hincal32619@yazilimlisesi.k12.tr>
2022-05-05 21:09:03 +0300

    Create Sidebar For Dashboard Navigation
    Sidebar Features Are:
    - Sidebar has 5 menus 
       1) Control Panel
       2) Contents
       3) Statics
       4) Settings
       5) Send Feedback
    at the moment these menus are not functional but they will be functional shortly.
    - Sidebar has 2 ways to interact with
       1) By clicking the button top left (which stands on navbar).
       2) By hovering over to sidebar.
    interacting navbar allows us to see all the names of the menus by expanding the sidebar
    - All the menus has an hover animation

--- client/pages/dashboard.js
+++ client/pages/dashboard.js
@@ -1,10 +1,17 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import {useState} from 'react'
 import styles from '../styles/Home.module.css'
 
+
 export default function Dashboard() {
-  return (
-   <div>
+
+    const [sideBar,setSideBar] = useState(false);
+
+    const toggleSideBar = () => setSideBar(!sideBar);
+
+
+    return (
+    <div>
        
        <Head>
         <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet' />
@@ -12,9 +19,11 @@ export default function Dashboard() {
 
        <div className="container">
 
-       <navbar className="flex items-center justify-between bg-zinc-800 w-screen h-10  box-border p-9 pr-5">
+       <navbar className="flex items-center justify-between bg-zinc-800 w-screen h-10  box-border pl-1.5 p-9 pr-5">
             <div className="flex"> 
-                <i className="bx bx-menu text-white mr-5 self-center text-4xl m-0 p-0"></i>
+                <button onClick={toggleSideBar} className="menuBars">
+                    <i className="bx bx-menu text-white mr-5 text-4xl m-0 p-0"></i>
+                </button>
                 <h1 className="text-5xl font-bold tracking-tight text-gray-900 self-center mb-2">
                   <span className="text-white">Post</span>
                   <span className="text-indigo-600">CMS</span>
@@ -31,11 +40,115 @@ export default function Dashboard() {
                 <button className="border border-indigo-600 text-indigo-600 font-bold text-lg rounded px-5 mr-5">
                     Create
                 </button>
-                <a><img src="https://this-person-does-not-exist.com/img/avatar-298ec0466599b1273254f8c5232ab3d2.jpg" className="rounded-full w-12 h-12"  /></a>
+                <a><img src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHByb2ZpbGV8ZW58MHx8MHx8&w=1000&q=80" className="rounded-full w-12 h-12"  /></a>
             </div>
-      </navbar>
-
+        </navbar>
+        <sidebar>
+            <div className={sideBar ? "sidebar" : " sidebar active"}>
+                <div className="h-1/5 flex align-center justify-around flex-col">
+                    <div className="flex justify-start w-full p-2">
+                        <i className="bx bx-layout text-zinc-500 self-center"></i>
+                        <p>Control Panel</p>
+                    </div>
+                    <div className="flex justify-start w-full p-2">
+                        <i className="bx bx-detail text-zinc-500 self-center"></i> 
+                        <p>Contents</p>
+                    </div>
+                    <div className="flex justify-start p-2">
+                        <i className="bx bx-bar-chart-square text-zinc-500 self-center"></i> 
+                        <p>Statics</p>
+                    </div>
+                </div>
+                <div className="mb-10">
+                    <div className="flex justify-start p-2">
+                        <i className="bx bx-cog text-zinc-500 self-center"></i> 
+                        <p>Settings</p>
+                    </div>
+                    <div className="flex justify-start p-2">
+                        <i className="bx bx-error text-zinc-500 self-center"></i> 
+                        <p>Send Feedback</p>
+                    </div>
+                </div>
+            </div>
+        </sidebar>
+    
        </div>
+       <style jsx>{`
+       .sidebar {
+            display:flex;
+            flex-direction:column;
+            position:fixed;
+            left:0;
+            transition:300ms;
+            height:95vh;
+            background-color: rgb(39 39 42);
+            width:3vw;
+            justify-content:space-between;            
+
+        }
+        .sidebar:hover{
+            transition:200ms;
+            width:12vw;
+        }
+        .sidebar p {
+            color:white;
+            opacity:0;
+            position:absolute;
+            display:inline;
+            font-size:1.3rem;
+            margin-left:0
+            z-index:1;
+        }
+        .sidebar img{
+            width:2vw;
+            height:4vh;
+            transition:300ms;
+        }
+        .sidebar i{
+            font-size:2rem;
+            z-index:2
+        }
+       .sidebar.active{
+            transition:200ms;
+            width:12vw;
+           
+        }
+        .sidebar.active p{
+            transform:scale(1);
+            position:static;
+            transition:300ms;
+            margin-left:10px;
+            opacity:1;
+        }
+        .sidebar.active img{
+            width:10vw;
+            height:20vh;
+            transition:200ms;
+        }
+        .sidebar div div{
+            
+        }
+        .sidebar div div:hover{
+            transition:200ms;
+            background-color:rgba(55, 48, 163,0.5);
+            border-left:10px solid rgb(55,48,163);
+        }
+        .sidebar:hover p{
+            transform:scale(1);
+            position:static;
+            transition:300ms;
+            margin-left:10px;
+            opacity:1;
+        }
+        .sidebar:hover img{
+            width:10vw;
+            height:20vh;
+            transition:200ms;
+        }
+        
+       `} 
+       </style>
+
    </div>
   )
 }
