c556cbdf15166109ac8282c473c0fd4ba2343bb3
Bekir Gulestan <gulestanbekir@gmail.com>
2022-05-05 14:08:27 +0300

    Implement Auth middlewares and Add NotAuthorized Error

--- backend/src/middlewares/require-auth.ts
+++ backend/src/middlewares/require-auth.ts
@@ -1,39 +1,14 @@
-import express, { Request, Response, NextFunction } from "express";
-import jwt from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
 import { NotAuthorizedError } from "../errors/not-authorized-error";
-
-declare global {
-  namespace Express {
-    interface Request {
-      currentUser?: UserPayload;
-    }
-  }
-}
-
-interface UserPayload {
-  id: string;
-  email: string;
-  iat: number;
-}
 
 export const requireAuth = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  if (req.session?.jwt) {
-    try {
-      const payload = jwt.verify(
-        req.session.jwt,
-        process.env.JWT_KEY!
-      ) as UserPayload;
-
-      req.currentUser = payload;
-      next();
-    } catch (err) {
-      throw new NotAuthorizedError();
-    }
-  } else {
+  if (!req.currentUser) {
     throw new NotAuthorizedError();
   }
+
+  next();
 };
