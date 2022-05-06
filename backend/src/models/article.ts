255d68e6d752eb28bee7978b158dad1f6f8d0854
Bekir Gulestan <gulestanbekir@gmail.com>
2022-05-07 00:16:56 +0300

    Change Article Schema
    Slug and Title Properties added to the schema. Time property is no more required and its automaticly sets now

--- backend/src/models/article.ts
+++ backend/src/models/article.ts
@@ -6,13 +6,22 @@ interface ArticleBlock {
   data: any;
 }
 
+/**
+ * Creation Requirements
+ */
 interface ArticleAttrs {
   userId: mongoose.Types.ObjectId;
-  time: number;
+
+  title: String;
+  slug: String;
+
   version: string;
   blocks: ArticleBlock[];
 }
 
+/**
+ * Query result object has these values
+ */
 interface ArticleDocument extends mongoose.Document {
   build(attrs: ArticleAttrs): ArticleDocument;
 }
@@ -26,9 +35,18 @@ const articleSchema = new mongoose.Schema({
     type: mongoose.Types.ObjectId,
     required: true,
   },
-  time: {
-    type: Number,
+  title: {
+    type: String,
     required: true,
+  },
+  slug: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  time: {
+    type: Date,
+    default: new Date(),
   },
   version: {
     type: String,
