7325856590ec2e9e7e595a64021625c0b4d99397
Bekir Gulestan <gulestanbekir@gmail.com>
2022-05-07 15:28:34 +0300

    ReDesign Dashboard Page
    and deleted module.css switched back to tailwind

--- client/pages/dashboard.js
+++ client/pages/dashboard.js
@@ -1,77 +1,140 @@
-import Head from 'next/head'
-import {useState} from 'react'
-import styles from '../styles/Dashboard.module.css'
+import Head from "next/head";
+import {
+  MenuIcon,
+  HomeIcon,
+  SearchIcon,
+  PlusCircleIcon,
+  CogIcon,
+  UserIcon,
+  LogoutIcon,
+  BellIcon,
+} from "@heroicons/react/outline";
+import { Popover } from "@headlessui/react";
+
+import { useState } from "react";
 
 export default function Dashboard() {
-
-    const [sideBar,setSideBar] = useState(false);
-
-    const toggleSideBar = () => setSideBar(!sideBar);
-
-
-    return (
+  const MenuData = [
+    {
+      position: "up",
+      title: "Home",
+      icon: HomeIcon,
+    },
+    {
+      position: "up",
+      title: "Explore",
+      icon: SearchIcon,
+    },
+    {
+      position: "up",
+      title: "Create New",
+      icon: PlusCircleIcon,
+    },
+    {
+      position: "down",
+      title: "Settings",
+      icon: CogIcon,
+    },
+  ];
+  return (
     <div>
-       
-       <Head>
-        <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet' />
-       </Head>
+      <Head>
+        <link
+          href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css"
+          rel="stylesheet"
+        />
+      </Head>
 
-       <div className="container">
+      <div className="flex w-full h-screen">
+        <div className="sm:flex sm:w-48 pt-16 flex-col hidden sm:flex-shrink-0 sm:flex-grow-0 bg-neutral-600 justify-between">
+          <div>
+            {MenuData.map((val) => {
+              if (val.position == "up")
+                return (
+                  <div
+                    key={val.title}
+                    className="text-white flex px-3  py-2 gap-2 items-center"
+                  >
+                    <val.icon className="w-6 h-6"></val.icon>
+                    <p>{val.title}</p>
+                  </div>
+                );
+            })}
+          </div>
 
-       <navbar className="flex items-center justify-between bg-zinc-800 w-screen h-10  box-border pl-1.5 p-9 pr-5">
-            <div className="flex"> 
-                <button onClick={toggleSideBar} className="menuBars">
-                    <i className="bx bx-menu text-white mr-5 text-4xl m-0 p-0"></i>
-                </button>
-                <h1 className="text-5xl font-bold tracking-tight text-gray-900 self-center mb-2">
-                  <span className="text-white">Post</span>
-                  <span className="text-indigo-600">CMS</span>
-                </h1>
+          <div>
+            {MenuData.map((val) => {
+              if (val.position == "down")
+                return (
+                  <div
+                    key={val.title}
+                    className="text-white flex px-3 py-2 gap-2 items-center"
+                  >
+                    <val.icon className="w-6 h-6"></val.icon>
+                    <p>{val.title}</p>
+                  </div>
+                );
+            })}
+          </div>
+        </div>
+
+        <div className="w-full h-16 bg-neutral-300 flex items-center p-4 justify-between">
+          <div className="text-lg font-mono flex items-center gap-x-2">
+            <div className="sm:hidden">
+              <Popover className="h-8">
+                <Popover.Button>
+                  <MenuIcon className="w-8 h-8"></MenuIcon>
+                </Popover.Button>
+                <Popover.Panel>
+                  <div className="absolute flex flex-col p-4 bg-neutral-200 rounded">
+                    {MenuData.map((val) => {
+                      return (
+                        <div
+                          className="flex gap-2 items-center"
+                          key={val.title}
+                        >
+                          <val.icon className="w-6 h-6"></val.icon>
+                          <p>{val.title}</p>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </Popover.Panel>
+              </Popover>
             </div>
+            Dashboard
+          </div>
 
-            <div className="searchBar bg-zinc-800 rounded pl-5 flex flex-row-reverse justify-end items-center w-7/12">
-                <input type="text" className="mb-1 transition duration-300 placeholder:text-zinc-500 px-5 py-2 rounded peer bg-zinc-800 text-zinc-500 w-screen focus:text-zinc-300 focus:outline-none focus:placeholder:text-zinc-800 " placeholder="Search in your profile..."></input>
+          <div className="flex items-center justify-between sm:w-32 h-16">
+            <p className="hidden sm:block">Bekogeko</p>
+            <Popover className="h-12">
+              <Popover.Button>
+                <img
+                  src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61"
+                  className="rounded-full w-12 h-12 hover:cursor-pointer"
+                />
+              </Popover.Button>
 
-                <i className="bx bx-search transition duration-300 text-lg text-zinc-500 peer-focus:text-zinc-300 self-center m-0 p-0"></i>
-            </div>
-
-            <div className="flex mr-5">
-                <button className="border border-indigo-600 text-indigo-600 font-bold text-lg rounded px-5 mr-5">
-                    Create
-                </button>
-                <a><img src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHByb2ZpbGV8ZW58MHx8MHx8&w=1000&q=80" className="rounded-full w-12 h-12"  /></a>
-            </div>
-        </navbar>
-        <sidebar>
-            <div className={sideBar ? styles.sidebar : styles.activeSidebar}>
-                <div className="h-1/5 flex align-center justify-around flex-col">
-                    <div className="flex justify-start w-full p-2">
-                        <i className="bx bx-layout text-zinc-500 self-center"></i>
-                        <p>Control Panel</p>
-                    </div>
-                    <div className="flex justify-start w-full p-2">
-                        <i className="bx bx-detail text-zinc-500 self-center"></i> 
-                        <p>Contents</p>
-                    </div>
-                    <div className="flex justify-start p-2">
-                        <i className="bx bx-bar-chart-square text-zinc-500 self-center"></i> 
-                        <p>Statics</p>
-                    </div>
+              <Popover.Panel className="w-16 h-16 absolute z-10 -translate-x-20">
+                <div className="w-32 bg-neutral-200 shadow-md p-2 rounded flex flex-col">
+                  <div className="flex gap-2 items-center py-1">
+                    <UserIcon className="w-6 h-6 "></UserIcon>
+                    <p>Profile</p>
+                  </div>
+                  <div className="flex gap-2 items-center py-1">
+                    <CogIcon className="w-6 h-6 "></CogIcon>
+                    <p>Settings</p>
+                  </div>
+                  <div className="flex gap-2 items-center py-1">
+                    <LogoutIcon className="w-6 h-6 "></LogoutIcon>
+                    <p>Log out</p>
+                  </div>
                 </div>
-                <div className="mb-10">
-                    <div className="flex justify-start p-2">
-                        <i className="bx bx-cog text-zinc-500 self-center"></i> 
-                        <p>Settings</p>
-                    </div>
-                    <div className="flex justify-start p-2">
-                        <i className="bx bx-error text-zinc-500 self-center"></i> 
-                        <p>Send Feedback</p>
-                    </div>
-                </div>
-            </div>
-        </sidebar>
-    
-       </div>
-   </div>
-  )
+              </Popover.Panel>
+            </Popover>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
