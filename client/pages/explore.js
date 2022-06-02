564c510fb1497feaab011ed4f6cbd53204b1511a
Bekogeko <gulestanbekir@gmail.com>
2022-06-03 01:08:37 +0300

    add Explore page key

--- client/pages/explore.js
+++ client/pages/explore.js
@@ -7,7 +7,7 @@ export default function ExplorePage({ data }) {
     <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
       {data.map((article) => {
         return (
-          <Link href={"/article/" + article.slug}>
+          <Link key={article._id} href={"/article/" + article.slug}>
             <div className="flex justify-center items-end w-full h-60 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md lg:h-40 xl:h-60 overflow-hidden group-hover:opacity-75 aspect-none hover:cursor-pointer bg-cover bg-[url('https://aquajogclub.com/wp-content/uploads/2019/12/placeholder.png')]">
               <div className="flex w-full h-2/6 justify-center bg-gray-300 rounded-md items-center">
                 <h3 className="text-xl truncate px-5">{article.title}</h3>
