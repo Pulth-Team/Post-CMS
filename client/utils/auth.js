c19d22bccfdd63121eab05cdcf28bbbcdb991022
Bekogeko <gulestanbekir@gmail.com>
2022-06-04 11:44:32 +0300

    Comment new auth util

--- client/utils/auth.js
+++ client/utils/auth.js
@@ -3,6 +3,7 @@ import Router from "next/router";
 import nextCookie from "next-cookies";
 import cookie from "js-cookie";
 
+// saving token to cookie and redirecting to /dashboard
 export const login = ({ token }) => {
   cookie.set("session", token, { expires: 1 });
   Router.push("/dashboard");
@@ -16,26 +17,37 @@ export const auth = (ctx) => {
 
   // If there's no token, it means the user is not logged in.
   if (!session) {
+    // if window is undefined it means we are in server side
     if (typeof window === "undefined") {
+      // set temporaly redirection and redirect to /login
       ctx.res.writeHead(302, { Location: "/login" });
+
+      // end response and send
       ctx.res.end();
     } else {
+      // if window is defined it means we are in client side  redirect to login via router
       Router.push("/login");
     }
-  }
+  } // if session is defined
 
+  // return session
   return session;
 };
 
+// this method only be called from client so window is always defined
 export const logout = () => {
+  // remove session from cookies
   cookie.remove("session");
   // to support logging out from all windows
   window.localStorage.setItem("logout", Date.now());
   Router.push("/login");
 };
 
+// middleware for making the page visible for only authenticated user
 export const withAuth = (WrappedComponent) => {
+  // wrapper component in react
   const Wrapper = (props) => {
+    // local storage event callback
     const syncLogout = (event) => {
       if (event.key === "logout") {
         console.log("logged out from storage!");
@@ -44,6 +56,7 @@ export const withAuth = (WrappedComponent) => {
     };
 
     useEffect(() => {
+      // add local storage event
       window.addEventListener("storage", syncLogout);
 
       return () => {
@@ -51,17 +64,22 @@ export const withAuth = (WrappedComponent) => {
         window.localStorage.removeItem("logout");
       };
     }, []);
+
+    // get layout if its defined in wrapped page if not use componenet itself
     const getLayout = WrappedComponent.getLayout || ((page) => page);
     return getLayout(<WrappedComponent {...props} />);
   };
 
   Wrapper.getInitialProps = async (ctx) => {
+    // get session if we have otherwise redirect automaticaly
     const session = auth(ctx);
 
+    // combine wrapped Pages initialProps and call method with both ctx
     const componentProps =
       WrappedComponent.getInitialProps &&
       (await WrappedComponent.getInitialProps(ctx));
 
+    // return with session and session (comes from auth method and always defined)
     return { ...componentProps, session };
   };
 
