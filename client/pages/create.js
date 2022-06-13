242a637809610df738032b016d42dc8e7809f060
Emir Taştan <emirt398@gmail.com>
2022-06-13 21:10:34 +0300

    Add create end-point to create page

--- client/pages/create.js
+++ client/pages/create.js
@@ -1,6 +1,7 @@
 import Dashboard from "../components/dashboard-layout";
 import useUser from "../hooks/use-user";
 import dynamic from "next/dynamic";
+import axios from "axios";
 
 import { useState } from "react";
 
@@ -13,13 +14,16 @@ export default function CreatePage() {
   const [editorInstance, setEditorInstance] = useState({});
   const [isReady, setIsReady] = useState(false);
 
+  const [title, setTitle] = useState("");
+
   return (
     <div key="ContentEditor" id="slugy">
       <div className="mb-3 pt-0">
         <input
           type="text"
-          placeholder="Placeholder"
-          className="px-3 py-4 placeholder-slate-300 text-slate-600 relative bg-white bg-white rounded text-base border-0 shadow outline-none focus:outline-none focus:ring w-full"
+          onChange={(event) => setTitle(event.target.value)}
+          placeholder="Title"
+          className="px-3 py-4 placeholder-slate-300 text-slate-600 relative bg-white rounded text-base border-0 shadow outline-none focus:outline-none focus:ring w-full"
         />
       </div>
       <hr></hr>
@@ -51,7 +55,15 @@ export default function CreatePage() {
         className="border p-2 rounded-md my-2"
         disabled={!isReady}
         onClick={async () => {
-          console.log(await editorInstance.save());
+          const savedata = await editorInstance.save();
+          delete savedata.time;
+          savedata.title = title;
+          const response = await axios.post("/api/article/create", {
+            title,
+            version: savedata.version,
+            blocks: savedata.blocks,
+          });
+          console.log(savedata);
         }}
       >
         Save
