d6a3a4ecd957d56d940d238cd41345890f4816a5
bekogeko <gulestanbekir@gmail.com>
2022-06-24 01:04:02 +0300

    installed moment.js for showing relative ago text. Remove Old Editor Page. Create project card.

--- client/pages/projects/index.js
+++ client/pages/projects/index.js
@@ -1,11 +1,30 @@
 import Dashboard from "../../components/dashboard-layout";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 import { FolderAddIcon, PlusIcon } from "@heroicons/react/outline";
 
 import useUser from "../../hooks/use-user";
 
+import ProjectCard from "../../components/project-card";
+
 export default function CreatePage() {
-  return (
+  const { userData, loaded } = useUser();
+  const [myArticles, setMyArticles] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      if (userData.id) {
+        const response = await axios.get("/api/article/user/" + userData.id);
+
+        console.log(response);
+        setMyArticles(response.data);
+      }
+    }
+    fetchData();
+  }, [loaded]);
+
+  const NoProjectComp = myArticles.length == 0 && (
     <div className="flex flex-col items-center">
       <FolderAddIcon className="stroke-1 w-12 stroke-slate-500"></FolderAddIcon>
       <div className="flex flex-col items-center my-4">
@@ -16,6 +35,18 @@ export default function CreatePage() {
         <PlusIcon className="w-4 inline-block "></PlusIcon>
         New Project
       </button>
+    </div>
+  );
+
+  const ProjectList = myArticles.map((article) => {
+    return <ProjectCard data={article} key={article._id} />;
+  });
+  return (
+    <div>
+      {NoProjectComp}
+      <div className="grid grid-cols-2 gap-2">
+        {myArticles.length > 0 && ProjectList}
+      </div>
     </div>
   );
 }
